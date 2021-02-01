068884040a345bd2ad5aa2e6f4bf8972e27b5fbc
alejandromn0711 <66174537+alejandromn0711@users.noreply.github.com>
2021-02-01 16:17:42 -0500

    Add files via upload

---
+++ Carrito.php
@@ -0,0 +1,89 @@
+<?php
+	if (!isset($_SESSION)) {
+	session_start();
+	}
+	require "PHP/config.php";
+    $mensaje = "";
+
+
+    if(isset($_POST['btnAccion'])){
+        switch($_POST['btnAccion']){
+            case 'Agregar':
+                
+                    if(is_numeric(openssl_decrypt( $_POST['codproducto'], COD, KEY))){
+                        $ID = openssl_decrypt( $_POST['codproducto'], COD, KEY);
+                        $mensaje.="ID Correcto... ".$ID."<br/>";
+                    }else{
+                        $mensaje.="Upss... ID Incorecto".$ID."<br/>";
+                    }
+
+
+                    if(is_string(openssl_decrypt( $_POST['nombre_producto'], COD, KEY))){
+                        $NOMBRE = openssl_decrypt( $_POST['nombre_producto'], COD, KEY);
+                        $mensaje.="Nombre ".$NOMBRE."<br/>";
+                    }else{
+                        $mensaje.="Upss... Algo paso con tu nombre".$NOMBRE."<br/>";
+                    }
+
+
+                    if(is_numeric(openssl_decrypt( $_POST['cantidad'], COD, KEY))){
+                        $CANTIDAD = openssl_decrypt( $_POST['cantidad'], COD, KEY);
+                        $mensaje.="Cantidad ".$CANTIDAD."<br/>";
+                    }else{
+                        $mensaje.="Upss... ID Incorecto".$CANTIDAD."<br/>";
+                    }
+
+
+                    if(is_numeric(openssl_decrypt( $_POST['precio'], COD, KEY))){
+                        $PRECIO = openssl_decrypt( $_POST['precio'], COD, KEY);
+                        $mensaje.="El precio es... ".$PRECIO."<br/>";
+                    }else{
+                        $mensaje.="Upss... ID Incorecto".$PRECIO."<br/>";
+                    }
+					if (!isset($_SESSION['CARRITO'])) {
+
+						$producto=array(
+							'codproducto'=>$ID,
+							'nombre'=>$NOMBRE,
+							'precio'=>$PRECIO,
+							'cantidad'=>$CANTIDAD
+						);
+							$_SESSION['CARRITO'][0]=$producto;
+                            $mensaje="Producto Agregado.";                            
+					}else{
+                        $id_productos=array_column($_SESSION['CARRITO'], 'codproducto');
+
+
+						$num_productos=count($_SESSION['CARRITO']);
+						$producto=array(
+							'codproducto'=>$ID,
+							'nombre'=>$NOMBRE,
+							'precio'=>$PRECIO,
+							'cantidad'=>$CANTIDAD		
+							);	
+						$_SESSION['CARRITO'][$num_productos]=$producto;
+                         $mensaje="Producto Agregado.";
+					}
+					//$mensaje=print_r( $_SESSION,true);
+                break;
+                case "Eliminar":
+                    if(is_numeric( openssl_decrypt( $_POST['codproducto'], COD, KEY))){
+                        $ID = openssl_decrypt( $_POST['codproducto'], COD, KEY);
+                
+                        foreach ($_SESSION['CARRITO'] as $indice => $producto) {
+                            if ($producto['codproducto']==$ID) {
+                                unset($_SESSION['CARRITO'][$indice]);
+                                echo "<script>alert('Elemento Borrado...');</script";
+                            }
+                        }
+                    }else{
+                        $mensaje.="Upss... ID Incorecto <br/>";
+                    }
+                    break;
+        }
+    }
+?>				
+
+
+
+	
---
+++ index.php
@@ -0,0 +1,146 @@
+<?php
+include 'Carrito.php';
+if (!isset($_GET["p"])) {
+  $p = "PaginaP";
+} else {
+  $p = $_GET["p"];
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Colchones Dulce Abrigo</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="iconos/fontawesome/css/all.css">
+  <link rel="stylesheet" type="text/css" href="css/catEstilos.css">
+</head>
+
+<body>
+
+  <!--BARRA DE NAVEGACION-->
+  <nav class="navbar navbar-expand-lg" style="border-bottom: solid 2px white;">
+
+    <a href="" class="navbar-brand"><img src="" alt="">Colchones Dulce Abrigo</a>
+
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item"><a href="index.php" class="nav-link active">Home</a></li>
+        <li class="nav-item"><a href="?p=Productos" class="nav-link">Productos</a></li>
+      
+
+
+
+
+        <?php
+        if (!isset($_SESSION)) {
+        session_start();
+        }
+        if (isset($_SESSION['active'])) {
+
+        ?>
+
+         <li class="nav-item"><a href="?p=MostrarCarrito" class="nav-link">Carro de compras (<?php echo (empty($_SESSION['CARRITO']))?0: count($_SESSION['CARRITO']); ?>)</a></li>
+          <li class="nav-item"><a href="Modulos/Logincliente.php" class="nav-link"><i class="fas fa-user-alt fa-1x"></i>&nbsp;&nbsp;<?php echo ucwords($_SESSION['nombre']); ?></a></li>
+          <li class="nav-item"><a href="?p=Salir" class="nav-link">Salir</a></li>
+
+
+        <?php
+        } else {
+
+        ?>
+
+          <li class="nav-item"><a href="Modulos/LoginADMI.php" class="nav-link">ADMI</a></li>
+          <li class="nav-item"><a href="Modulos/Logincliente.php" class="nav-link">Iniciar sesion</a></li>
+
+        <?php
+        }
+        ?>
+      </ul>
+
+
+
+
+
+
+    </div>
+  </nav>
+  <!-- FIN BARRA DE NAVEGACION-->
+
+
+  <div class="cuerpo">
+
+    <?php
+    if (file_exists("Modulos/" . $p . ".php")) {
+      include "Modulos/" . $p . ".php";
+    } else {
+      echo "modulo desconocido";
+    }
+
+    ?>
+
+  </div>
+
+
+
+  <!--FOOTER-->
+
+  <footer>
+    <div class="container">
+      <div class="row text-light text-center py-4 justify-content-center">
+
+        <div class="col-sm-10 col-md-8 col-lg-6">
+          <img src="img/logo1.png" alt="">
+          <p>ooooooooootro parrafo...</p>
+          <ul class="social pt-3">
+            <li><a href="#" target="_blank"><i class="fab fa-facebook"></i></a></li>
+            <li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </footer>
+
+  <!--FIN FOOTER-->
+
+  <!--PARTE DOS FOOTER-->
+
+  <div class="socket text-light text-center py-3">
+    <p>&copy; <a href="index.php" target="_blank">colchonesdulceabrigo.com</a></p>
+  </div>
+
+  <!-- FIN PARTE DOS FOOTER-->
+
+
+  <!--FIN PAG-->
+
+
+
+  </div>
+
+
+</body>
+
+</html>
+
+<script src="js/jquery-3.5.1.min.js"></script>
+<script src="js/all.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.bundle2.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+<script>
+    $(function () {
+            $('[data-toggle="popover"]').popover()
+   })
+</script>
