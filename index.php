9af4203454d981aabbd32fb4617965e5f198a339
davidalejandro1907 <davidalejandrorivera2525@gmail.com>
2021-03-06 17:02:36 -0500

    Carrusel no avanza (Solucionado)

--- index.php
+++ index.php
@@ -137,8 +137,9 @@ if (!isset($_GET["p"])) {
 
 <script src="js/jquery-3.5.1.min.js"></script>
 <script src="js/all.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="js/bootstrap.bundle2.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script src="js/bootstrap.bundle2.min.js"></script>
+
 <script>
   $(function() {
     $('[data-toggle="popover"]').popover()
