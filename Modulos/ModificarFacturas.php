7d86edab486af8d8035e852dfd3c9ef80644494a
alejandromn0711 <66174537+alejandromn0711@users.noreply.github.com>
2021-08-24 00:51:47 -0500

    facturasAdmin

---
+++ Modulos/ModificarFacturas.php
@@ -0,0 +1,54 @@
+<head>
+    <link rel="stylesheet" type="text/css" href="css/styleFacturasAd.css">
+</head>
+
+<body>
+    <header class="clearfix">
+        <div id="logo">
+            <img style="width:180px;" src="img/1600738901566.png">
+        </div>
+        <h1>FACTURA N° </h1>
+        <div id="company" class="clearfix">
+            <div>Dulce Abrigo</div>
+            <div>Alfonso Lopez,<br /> Usme</div>
+            <div>AsistenciaDulceAbrigo@gmail.com</div>
+        </div>
+        <div id="project">
+            <div><span>N° DE VENTA:</span></div>
+            <div><span>CLIENTE: </span></div>
+            <div><span>DIRECCION: </span></div>
+            <div><span>EMAIL: </span></div>
+            <div><span>FECHA: </span></div>
+            <div><span>ESTATUS: </span></div>
+        </div>
+    </header>
+    <main>
+        <table>
+            <thead>
+                <tr>
+                    <th class="service">ID</th>
+                    <th class="desc">PRODUCTO</th>
+                    <th>PRECIO</th>
+                    <th>CANTIDAD</th>
+                    <th>TOTAL</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+
+                    <td class="service"></td>
+                    <td class="desc"></td>
+                    <td class="unit"></td>
+                    <td class="qty"></td>
+                    <td class="total"></td>
+                </tr>
+                <tr style="text-align: center;">
+                    <td><b> TOTAL </b></td>
+                    <td> - </td>
+                    <td> - </td>
+                    <td> - </td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+</body>
